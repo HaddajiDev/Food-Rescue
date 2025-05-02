4d628ca828335f1c41591b8761e08e3f6cc74540
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-05-02 16:29:00 +0100

    fix

--- api/index.js
+++ api/index.js
@@ -6,11 +6,20 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const allowedOrigins = [
+    "https://foodrescue-1.vercel.app"
+];
+  
 app.use(cors({
-    origin: "*",
-    credentials: true
+      origin: (origin, callback) => {
+          if (!origin || allowedOrigins.includes(origin)) {
+              callback(null, true);
+          } else {
+              callback(new Error("Not allowed by CORS"));
+          }
+      },
+      credentials: true
 }));
-
 
 app.use('/get', require('./router/pics'));
 app.get("/", (req, res) => res.send("Working"));
